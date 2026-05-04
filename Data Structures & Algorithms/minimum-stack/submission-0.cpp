fc5d6f928c13c1d669a2deaee2e263759ed8eeaf
Gautam Agrawal <agrawalgautam111@gmail.com>
2026-05-04 22:27:12 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,29 @@
+class MinStack {
+public:
+
+    stack<int>a,b;
+    MinStack() {
+    }
+    
+    void push(int val) {
+        a.push(val);
+        if(b.empty())
+            b.push(val);
+        else {
+            b.push(min(b.top(),val));
+        }
+    }
+    
+    void pop() {
+        a.pop();
+        b.pop();
+    }
+    
+    int top() {
+        return a.top();
+    }
+    
+    int getMin() {
+        return b.top();
+    }
+};
